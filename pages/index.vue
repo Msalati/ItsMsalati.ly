5391344a4a5cc3cf64dcca73852e6d872ef7dc85
Msalati <ItsMsalati@gmail.com>
2022-12-31 18:06:39 +0200

    development started

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,16 @@
 <template>
-  <Tutorial/>
+  <div>
+    <h1>hello world</h1>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
-}
+  name: "IndexPage",
+};
 </script>
+<style>
+h1 {
+  text-align: center;
+}
+</style>
