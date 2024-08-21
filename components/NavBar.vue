9d39674f1ff348322b58c934aeb22d454b9bf82f
Msalati <ItsMsalati@gmail.com>
2024-08-21 03:22:11 +0200

    work-inprogress

---
+++ components/NavBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <nav>
+    <ul class="nav-items">
+      <li>h</li>
+      <li>
+        <img width="200px" height="200px" src="assets/svgs/ItsMsalati-words.svg" alt="">
+      </li>
+      <li>c</li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+  .nav-items{
+    color: white;
+    display: flex;
+    justify-content: space-between;
+    list-style: none;
+    margin: 0;
+    padding: 0 40;
+
+  }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,56 @@
 <template>
   <div>
-    <h1>hello world</h1>
+    <nav-bar />
+    <div class="circle-parent">
+      <div class="circle">
+
+      </div>
+    </div>
   </div>
-</template>
+</div></template>
 
 <script>
+import NavBar from "~/components/NavBar.vue";
+
 export default {
   name: "IndexPage",
+  components: { NavBar },
 };
 </script>
 <style>
+@import url("../assets/css/style.css");
+
 h1 {
   text-align: center;
+}
+
+.circle-parent {
+
+  display: flex;
+  justify-content: center;
+}
+
+.circle {
+  background: linear-gradient(45deg, #000000, #3DF2F2);
+  background-size: 400% 400%;
+  animation: gradient 2s ease-in infinite;
+  border-radius: 50%;
+  height: 25vw;
+  width: 25vw;
+  @media screen and (max-width: 600px) {
+    height: 50vw;
+    width: 50vw;
+  }
+}
+
+@keyframes gradient {
+
+  0% {
+    background-position: 0% 0%;
+  }
+
+  100% {
+    background-position: 100% 100%;
+  }
 }
 </style>
